1e8522921d0ca2b6825c77457170d8851660934d
som-syom <dhyeon@student.42seoul.kr>
2022-02-28 17:10:11 +0900

    feat - main 변경

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import TodoList from "./components/TodoList";
 import TodoForm from "./components/TodoForm";
@@ -7,9 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <TodoList />
         <TodoForm />
+        <input type="date" value={new Date()} />
       </header>
     </div>
   );
